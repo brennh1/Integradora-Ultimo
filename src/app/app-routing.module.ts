21b269215f3257dff133a5d50ad7280a494b03bb
Brenda <Brenn>
2024-08-07 12:36:29 -0600

    Ultima version

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './Componentes/home/home.component';
+import { AdministradorComponent } from './Componentes/home/administrador/administrador.component';
+import { ComentariosComponent } from './Componentes/home/comentarios/comentarios.component';
+import { ControlUsuariosComponent } from './Componentes/home/control-usuarios/control-usuarios.component';
+import { DocentesComponent } from './Componentes/home/docentes/docentes.component';
+import { LoginComponent } from './Componentes/home/login/login.component';
+import { UsuarioComponent } from './Componentes/home/usuario/usuario.component';
+import { VerificacionComponent } from './Componentes/home/verificacion/verificacion.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'home',component:HomeComponent},
+  {path:'admin',component:AdministradorComponent},
+  {path:'comen',component:ComentariosComponent},
+  {path:'control',component:ControlUsuariosComponent},
+  {path:'doce',component:DocentesComponent},
+  {path:'login',component:LoginComponent},
+  {path:'usu',component:UsuarioComponent},
+  {path:'veri',component:VerificacionComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Componentes/home/home.component';
+import { AdministradorComponent } from './Componentes/home/administrador/administrador.component';
+import { ComentariosComponent } from './Componentes/home/comentarios/comentarios.component';
+import { ControlUsuariosComponent } from './Componentes/home/control-usuarios/control-usuarios.component';
+import { DocentesComponent } from './Componentes/home/docentes/docentes.component';
+import { LoginComponent } from './Componentes/home/login/login.component';
+import { UsuarioComponent } from './Componentes/home/usuario/usuario.component';
+import { VerificacionComponent } from './Componentes/home/verificacion/verificacion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AdministradorComponent,
+    ComentariosComponent,
+    ControlUsuariosComponent,
+    DocentesComponent,
+    LoginComponent,
+    UsuarioComponent,
+    VerificacionComponent
   ],
   imports: [
     BrowserModule,
